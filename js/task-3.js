a40c39b35e37172b4c82fd426e7dcddb749a3e51
Alexander <vasilenkobox@gmail.com>
2024-11-29 15:53:33 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 // Напиши стрілочну функцію sortByDescendingFriendCount(users) , яка прийматиме один параметр users — масив об’єктів користувачів.
 // Функція має повертати масив усіх користувачів, відсортованих за спаданням кількостій їх друзів (властивість friends).
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
-const sortByDescendingFriendCount = (users) => users.toSorted((a, b) => a.friends - b.friends);
+const sortByDescendingFriendCount = (users) => users.toSorted((a, b) => a.friends.length - b.friends.length);
 console.log(
   sortByDescendingFriendCount([
     {
